1ec6be7f4f2adc58792a9527401cba3e25b1ec80
Sri Bhuvana Vaishnavi Dasika <81732369+Dasika-Vaishnavi@users.noreply.github.com>
2024-03-11 01:57:35 +0530

    Update e_common_traits.rs 2024

--- src/e_common_traits.rs
+++ src/e_common_traits.rs
@@ -20,9 +20,16 @@ pub struct Employee {
 
 // We want to consider two employee instances equal iff they have the same `uid`.
 
+impl std::fmt::Display for Employee {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        write!(f, "Employee[name={}, experience={}, wage={}, uid={}]", self.name, self.experience, self.wage, self.uid)
+    }
+}
+
 impl PartialEq for Employee {
 	fn eq(&self, other: &Self) -> bool {
-		todo!("complete the implementation");
+		// todo!("complete the implementation");
+    self.uid == other.uid    
 	}
 }
 impl Eq for Employee {}
@@ -32,18 +39,32 @@ impl Eq for Employee {}
 // bring to the company. Value is defined as the quotient of the experience they've acquired
 // at the company divided by their wage. Use integer division for the purpose of this calculation.
 
+// impl PartialOrd for Employee {
+// 	fn partial_cmp(&self, other: &Self) -> Option<std::cmp::Ordering> {
+// 		// todo!("complete the implementation");
+//         if self.uid == other.uid {
+//             // Some(Ordering::Equal)
+// 			return Some(Ordering::Equal);
+//         } else {
+// 			let self_value = self.experience / (self.wage.max(1)); // Avoid division by zero
+// 			let other_value = other.experience / (other.wage.max(1));
+// 			self_value.partial_cmp(&other_value)
+//         }
+// 	}
+// }
+
 impl PartialOrd for Employee {
-	fn partial_cmp(&self, other: &Self) -> Option<std::cmp::Ordering> {
-		todo!("complete the implementation");
-	}
+    fn partial_cmp(&self, other: &Self) -> Option<Ordering> {
+        Some(self.uid.cmp(&other.uid))
+    }
 }
 
 impl Ord for Employee {
 	fn cmp(&self, other: &Self) -> std::cmp::Ordering {
-		todo!("complete the implementation");
-	}
+		// todo!("complete the implementation");
+        self.partial_cmp(other).unwrap_or(Ordering::Equal)
 }
-
+}
 // We want to parse employee information from a string data
 // The string data should be comma separated. Here are a few examples:
 //
@@ -59,14 +80,26 @@ impl TryFrom<String> for Employee {
 	type Error = &'static str;
 
 	fn try_from(value: String) -> Result<Self, Self::Error> {
-		todo!("complete the implementation");
+		// todo!("complete the implementation");
+        let parts: Vec<&str> = value.split(',').map(|s| s.trim()).collect();
+        if parts.len() != 4 {
+            return Err("Invalid input format");
+        }
+
+        let name = parts[0].to_string();
+        let experience = parts[1].parse::<u32>().map_err(|_| "Invalid experience format")?;
+        let wage = parts[2].parse::<u32>().map_err(|_| "Invalid wage format")?;
+        let uid = parts[3].parse::<u32>().map_err(|_| "Invalid uid format")?;
+
+        Ok(Employee { name, experience, wage, uid })
 	}
 }
 
 // We also want to convert employees back into strings in the same format as above.
 impl From<Employee> for String {
 	fn from(e: Employee) -> Self {
-		todo!("complete the implementation");
+		// todo!("complete the implementation");
+        format!("{},{},{},{}", e.name, e.experience, e.wage, e.uid)
 	}
 }
 
@@ -74,13 +107,16 @@ impl From<Employee> for String {
 /// On a scale from 0 - 255, with zero being extremely easy and 255 being extremely hard,
 /// how hard did you find this section of the exam.
 pub fn how_hard_was_this_section() -> u8 {
-	todo!()
+	// todo!()
+  75
+  
 }
 
 /// This function is not graded. It is just for collecting feedback.
 /// How much time (in hours) did you spend on this section of the exam?
 pub fn how_many_hours_did_you_spend_on_this_section() -> u8 {
-	todo!()
+	// todo!()
+  1
 }
 
 #[cfg(test)]
@@ -147,4 +183,32 @@ mod tests {
 
 		assert!(susie != billy);
 	}
+}
+
+use std::cmp::Ordering;
+use std::convert::TryFrom;
+
+fn main() {
+    // Example of converting an Employee instance back into a string
+    let billy = Employee {
+        name: "Billy".to_string(),
+        experience: 4,
+        wage: 5,
+        uid: 345,
+    };
+    let employee_string: String = billy.to_string();
+    println!("Converted employee to string: {}", employee_string);
+
+    // Comparing two employees
+    let susie = Employee {
+        name: "Susie".to_string(),
+        experience: 5,
+        wage: 5,
+        uid: 347,
+    };
+    if susie > billy {
+        println!("Susie has a higher value to the company than Billy");
+    } else {
+        println!("Billy has a higher value to the company than Susie");
+    }
 }
